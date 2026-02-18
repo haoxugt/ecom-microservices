45d5143c2d0814130150a717695675bf31503a49
Hao Xu <haoxugt@gmail.com>
2026-02-18 13:43:05 -0800

    Set up Cart Entity and define adding Product to Cart Endpoint

--- src/main/java/com/app/ecom/controller/CartController.java
+++ src/main/java/com/app/ecom/controller/CartController.java
@@ -1,11 +1,28 @@
 package com.app.ecom.controller;
 
+import com.app.ecom.dto.CartItemRequest;
+import com.app.ecom.service.CartService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/carts")
+@RequestMapping("/api/cart")
 public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping
+    public ResponseEntity<String> addToCart(
+            @RequestHeader("X-User-ID") String userId,
+            @RequestBody CartItemRequest request){
+        boolean added = cartService.addToCard(userId, request);
+        if (!added) {
+            return ResponseEntity.badRequest()
+                    .body("Product Out of Stock or User not found or Product not found");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
---
+++ src/main/java/com/app/ecom/service/CartService.java
@@ -0,0 +1,64 @@
+package com.app.ecom.service;
+
+import com.app.ecom.dto.CartItemRequest;
+import com.app.ecom.model.CartItem;
+import com.app.ecom.model.Product;
+import com.app.ecom.model.User;
+import com.app.ecom.repository.CartItemRepository;
+import com.app.ecom.repository.ProductRepository;
+import com.app.ecom.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+    private final ProductRepository productRepository;
+    private final CartItemRepository cartItemRepository;
+    private final UserRepository userRepository;
+
+    public boolean addToCard(String userId, CartItemRequest request) {
+        // Look for product
+        Optional<Product> productOpt = productRepository.findById(request.getProductId());
+
+        if (productOpt.isEmpty())
+            return false;
+
+        Product product = productOpt.get();
+
+        // Check if the stock quantity is valid
+        if (product.getStockQuantity() < request.getQuantity())
+            return false;
+
+        // Look for user
+        Optional<User> userOpt = userRepository.findById(Long.valueOf(userId));
+
+        if (userOpt.isEmpty())
+            return false;
+
+        User user = userOpt.get();
+
+        // Check if the product is already in the cart
+        CartItem existingCartItem = cartItemRepository.findByUserAndProduct(user, product);
+        if (existingCartItem != null) {
+            // Update the quantity
+            existingCartItem.setQuantity(existingCartItem.getQuantity() + request.getQuantity());
+            existingCartItem.setPrice(product.getPrice().multiply(BigDecimal.valueOf(existingCartItem.getQuantity())));
+            cartItemRepository.save(existingCartItem);
+        } else {
+            // Create new cart item
+            CartItem cartItem = new CartItem();
+            cartItem.setUser(user);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(request.getQuantity());
+            cartItem.setPrice(product.getPrice().multiply(BigDecimal.valueOf(request.getQuantity())));
+            cartItemRepository.save(cartItem);
+
+        }
+
+        return true;
+    }
+}
